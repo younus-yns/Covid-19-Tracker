3cc42cb99632b346b045932dd03fccb748326cee
younus-yns <61771878+younus-yns@users.noreply.github.com>
2020-05-22 12:37:50 +0300

    all folders have been added

---
+++ build/precache-manifest.b39c21b5431c3a3e643e0dbf739be720.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10ce06b392f98b100c1572caf77536d0",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "7e15c6b155545c0cf1e2",
+    "url": "/Covid-19-Tracker/static/css/main.b0931e10.chunk.css"
+  },
+  {
+    "revision": "2476933cd90be351dfb8",
+    "url": "/Covid-19-Tracker/static/js/2.009dd565.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-Tracker/static/js/2.009dd565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e15c6b155545c0cf1e2",
+    "url": "/Covid-19-Tracker/static/js/main.8fc70506.chunk.js"
+  },
+  {
+    "revision": "5d9c7173b1e41f7bf515",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.ec752871.js"
+  },
+  {
+    "revision": "2289adda20d2d953c834f7cc5a5be14c",
+    "url": "/Covid-19-Tracker/static/media/image.2289adda.png"
+  }
+]);
